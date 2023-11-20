7a4389c61603dc0d86216edfd996902a5e4162cb
Luke <lwithers93@live.co.uk>
2023-11-20 14:21:42 +0000

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import Game from "./App.js";
-import BrowserRouter from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
 root.render(

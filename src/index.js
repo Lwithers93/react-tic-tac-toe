52cf094d101fb5f1f75a001b2c46e6d9d82fb64f
Luke <lwithers93@live.co.uk>
2023-11-20 14:15:56 +0000

    update index.js and json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-
 import Game from "./App.js";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(
+  <BrowserRouter basename={{process.env.PUBLIC_URL}}>
+    <Game />
+  </BrowserRouter>
+);
